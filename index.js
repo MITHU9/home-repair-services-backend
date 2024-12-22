7d72119d85594580be3297683c6e6c3e92728a7c
Shahariar Mithu <kmmithu2015@gmail.com>
2024-12-22 16:25:13 +0600

    add service details api and booked service  api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -38,6 +38,10 @@ async function run() {
       .db("homeRepairStore")
       .collection("services");
 
+    const bookedServiceCollection = client
+      .db("homeRepairStore")
+      .collection("bookedServices");
+
     //auth related APIs
     app.post("/jwt", (req, res) => {
       const user = req.body;
@@ -71,10 +75,25 @@ async function run() {
       res.send(services);
     });
 
+    // Get a single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
     // Add a new service
     app.post("/add-service", async (req, res) => {
       const service = req.body;
       const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    });
+
+    // booked services
+    app.post("/book-service", async (req, res) => {
+      const service = req.body;
+      const result = await bookedServiceCollection.insertOne(service);
       res.send(result);
     });
 
